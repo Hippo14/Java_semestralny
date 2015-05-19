ed09b27729eab166ed3f5812bff6e2ed121a6feb
Hippo14 <hippo14@gmail.com>
2015-05-19 20:04:54 +0200

    Build v0.01
    Przyciski bez akcji

--- app/src/main/java/pz2015/habits/java_semestralny/MainActivity.java
+++ app/src/main/java/pz2015/habits/java_semestralny/MainActivity.java
@@ -4,14 +4,34 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private static final int NUM_ROWS = 2;
+    private static final int NUM_COLS = 3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        populateButtons();
+    }
+
+    private void populateButtons() {
+        TableLayout table = (TableLayout) findViewById(R.id.tableForButtons);
+        for (int row = 0; row < NUM_ROWS; row++) {
+            TableRow tableRow = new TableRow(this);
+            table.addView(tableRow);
+
+            for (int col = 0; col < NUM_COLS; col++) {
+                Button button = new Button(this);
+                tableRow.addView(button);
+            }
+        }
     }
 
 
